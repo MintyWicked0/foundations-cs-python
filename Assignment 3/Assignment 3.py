d2beabd729a9ef0571a4f5a718a4b65cae1c4aa6
MintyWicked0 <mintywicked@gmail.com>
2023-10-29 18:15:56 +0200

    Update

--- Assignment 3/Assignment 3.py
+++ Assignment 3/Assignment 3.py
@@ -1,39 +1,102 @@
 def matracies():
-    rows = int(input("Enter number of rows: "))    #User will input the desired
-    cols = int(input("Enter number of columns: ")) #size of the matrix
-    mat1 = [] #
-    mat2 = [] #creating empty matrices
-    result = []  #
-
+    # Get the size of the matrices by the user input
+    rows = int(input("Enter number of rows: "))
+    cols = int(input("Enter number of columns: "))
+    
+   # Create matrices that user will fill
+    matrix1 = []
+    matrix2 = []
+    result = []
+    
+    
+    # Get the elements of the first matrix  
+    print("Enter elements for the first matrix:")
     for i in range(rows):
-        mat1_row = [0] * cols
-        mat2_row = [0] * cols
-        result_row = [0] * cols
-
-        mat1.append(mat1_row)
-        mat2.append(mat2_row)
+        row = []
+        print("Enter elements for row", (i + 1),":")
+        elements = input().split()
+        for element in elements:
+            row.append(int(element))
+        matrix1.append(row)
+   
+    # Get elements for the second matrix
+    print("Enter elements for the second matrix:")
+    for i in range(rows):
+        row = []
+        print("Enter elements for row", (i + 1),":")
+        elements = input().split()
+        for element in elements:
+            row.append(int(element))
+        matrix2.append(row)
+    
+    # Add the matrices
+    for i in range(rows):
+        result_row = []
+        for j in range(cols):
+            result_row.append(matrix1[i][j] + matrix2[i][j])
         result.append(result_row)
     
-    for i in range(rows):
-        for j in range(cols):
-            result[i][j] = mat1[i][j] + mat2[i][j]
-            
+    # Prints the resulting matrix calculatedd
     print("The resulting matrix is:")
     for row in result:
         print(row)
     
-def checkingR():
-    mat1 = []
-    mat2 = []
 
-    print("Enter elements of the first matrix:")
+def checkingR():
+    # Initialize empty matrices
+    matrix1 = []
+    matrix2 = []
+
+    # Get elements for the first matrix separated values with spaces and press Enter after each row. Leave an empty line to finish
+    print("Enter elements of the first matrix: ")
+
+    while True:
+        row_input = input()
+        
+        if row_input:
+           row = []
+           elements = row_input.split()
+        
+           for element in elements:
+               row.append(int(element))
+        
+           matrix1.append(row)
+        else:
+            break
+
+    # Get elements for the second matrix separated values with spaces and press Enter after each row. Leave an empty line to finish
+    print("Enter elements of the second matrix: ")
+
+    while True:
+        row_input = input()
+    
+        if row_input:
+            row = []
+            elements = row_input.split()
+        
+            for element in elements:
+                row.append(int(element))
+        
+            matrix2.append(row)
+        else:
+            break
+
+    #Check if one matrix is a rotation of the other
+    transposed_matrix1 = [list(row) for row in zip(*matrix1)]
+    transposed_matrix2 = [list(row) for row in zip(*matrix2)]
+
+    if transposed_matrix1 == matrix2 or transposed_matrix2 == matrix1:
+        print("The matrices are rotations of each other.")
+    else:
+        print("The matrices are not rotations of each other.")
+
 
 def invertion():
     ori = {}
     inv = {}
 
     n = int(input("Enter the number of key-value pairs: "))
-    
+
 def conversation():
     user_data = []
 
@@ -43,11 +106,16 @@ def conversation():
         user = []
         user.append(input("Enter First Name: "))
         user.append(input("Enter Last Name: "))
-        user.append(input
+        user.append(input("Enter ID: "))
+        user.append(input("Enter Job Title: "))
+        user.append(input("Enter Company: "))
+        user_data.append(user)
+
+    user_dict = ()
 
 def checkingP():
     print("Working 5")
-    
+
 def search():
     print("Working 6")
     
