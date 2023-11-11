36b35ec08f222751539c23468063579930ba610b
MintyWicked0 <mintywicked@gmail.com>
2023-11-11 21:42:20 +0200

    Update added opening and closing tab with minor changes for the menu

--- Midterm/Malek Ramadan Midterm.py
+++ Midterm/Malek Ramadan Midterm.py
@@ -1,6 +1,42 @@
+def open_tab(tabs):
+    title = input("Enter the title of the website: ")
+    url = input("Enter the URL of the website: ")
+    tab = {"title": title, "url": url}
+    tabs.append(tab)
+    print(title, "opened successfully.")
+
+def close_tab(tabs):
+    index = input("Enter the index of the tab you wish to close: ")
+    if index.isdigit():
+        index = int(index) - 1
+        if 0 <= index < len(tabs):
+            closed_tab = tabs.pop(index)
+            print("Closed ", closed_tab['title'])
+        else:
+            print("Invalid Tab selection. No tab closed. Check ")
+    
+def switch_tab():
+    print(" ")
+    
+def display_all_tabs():
+    print(" ")
+
+def nested_tab():
+    print(" ")
+    
+def clear_all_tabs():
+    print(" ")
+
+def save_tabs():
+    print(" ")
+    
+def import_tabs():
+    print(" ")
+    
 def main():
+    tabs= []
+    
     while True:
-        print("\nMenu:")
         print("1. Open Tab")
         print("2. Close Tab")
         print("3. Switch Taab")
@@ -8,27 +44,30 @@ def main():
         print("5. Open nested tabs")
         print("6. Clear All Tabs")
         print("7. Save Tabs")
-        print("8. Exit")
+        print("8. Import Tabs")
         print("9. Exit")
         choice = input("Enter your choice: ")
         
         if choice == '1':
-            
+            open_tab(tabs)
         elif choice == '2':
-            
+            close_tab(tabs)
         elif choice == '3':
-            
+            switch_tab()
         elif choice == '4':
-            
+            display_all_tabs()
         elif choice == '5': 
-            
+            nested_tab()
         elif choice == '6':
-            
+            clear_all_tabs()
         elif choice == '7':
-        
+            save_tabs()
         elif choice == '8':
-            
+            import_tabs()
         elif choice == '9':
-            print("End of the Program!")
+            print("End of the Program! \nBye!")
+            break
+        else:
+            print("This number is out of range! \nPlease enter a valid number from 1 to 9.")
                
 main()
