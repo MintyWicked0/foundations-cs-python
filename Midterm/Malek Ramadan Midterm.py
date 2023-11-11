e41897dbfc727f87e872fb84d21628c10c6c88bd
MalekR <mintywicked@gmail.com>
2023-11-11 05:22:25 +0200

    Midterm

---
+++ Midterm/Malek Ramadan Midterm.py
@@ -0,0 +1,34 @@
+def main():
+    while True:
+        print("\nMenu:")
+        print("1. Open Tab")
+        print("2. Close Tab")
+        print("3. Switch Taab")
+        print("4. Displat all tabs")
+        print("5. Open nested tabs")
+        print("6. Clear All Tabs")
+        print("7. Save Tabs")
+        print("8. Exit")
+        print("9. Exit")
+        choice = input("Enter your choice: ")
+        
+        if choice == '1':
+            
+        elif choice == '2':
+            
+        elif choice == '3':
+            
+        elif choice == '4':
+            
+        elif choice == '5': 
+            
+        elif choice == '6':
+            
+        elif choice == '7':
+        
+        elif choice == '8':
+            
+        elif choice == '9':
+            print("End of the Program!")
+               
+main()
