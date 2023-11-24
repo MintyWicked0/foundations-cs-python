98ea67e20633b0f52861b21af7d7874ba46de0ef
MintyWicked0 <mintywicked@gmail.com>
2023-11-24 23:37:36 +0200

    Added Linked List Class

--- Assignment 4/Assignment 4.py
+++ Assignment 4/Assignment 4.py
@@ -2,6 +2,15 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_node(self, value):
+        new_node = Node(value)
+        if self.head:
+            self.head = new_node
 
 class Student:
     def __init__(self,name,grade,midterm_grade,final_grade,good_attitude):
